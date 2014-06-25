99307a2702290cd1c83ca5cc5d90876661d686a9
jgautsch <jongautsch@me.com>
2014-06-25 10:45:49 -0500

    convert tabs to spaces

--- lib/srfax.rb
+++ lib/srfax.rb
@@ -4,42 +4,42 @@ require "httmultiparty"
 module Srfax
   class API
 
-  	include ::HTTMultiParty
+    include ::HTTMultiParty
 
-  	base_uri 'https://www.srfax.com'
-  	headers 'User-Agent' => "hello_fax gem #{VERSION}"
+    base_uri 'https://www.srfax.com'
+    headers 'User-Agent' => "hello_fax gem #{VERSION}"
 
-  	API_ENDPOINT = '/SRF_SecWebSvc.php'
+    API_ENDPOINT = '/SRF_SecWebSvc.php'
 
-  	attr_accessor :guid
+    attr_accessor :guid
 
-  	def initialize(username, password, guid)
-  	  self.class.basic_auth username, password
-  	  @guid = guid
-  	end
+    def initialize(username, password, guid)
+      self.class.basic_auth username, password
+      @guid = guid
+    end
 
-  	# Queues a fax for sending
-  	# Expected response:
-  	# {
-  	# 	"Status": either "Success" or "Failed",
-  	# 	"Result": Queued Fax ID (FaxDetailsID) or Reason for failure
-  	# }
-  	def send_fax(to, file)
-  		self.class.post(
-  			API_ENDPOINT,
-  			query: {
-  				action: 					'Queue_Fax',
-  				access_id: 				@access_id,
-  				access_pwd: 			@access_pwd,
-  				sCallerId: 				@sender_fax_number,
-  				sSenderEmail: 		@sender_email,
-  				sFaxType: 				'SINGLE', # TODO: make this "SINGLE" or "BROADCAST" optionally
-  				sToFaxNumber: 		to,
-  				sResponseFormat: 	'JSON',
-  				sRetries: 				3,
-  				sFileName_1: 			'preferral_file',
-  				sFileContent_1: 	file
-  			})
-  	end
+    # Queues a fax for sending
+    # Expected response:
+    # {
+    #   "Status": either "Success" or "Failed",
+    #   "Result": Queued Fax ID (FaxDetailsID) or Reason for failure
+    # }
+    def send_fax(to, file)
+      self.class.post(
+        API_ENDPOINT,
+        query: {
+          action:           'Queue_Fax',
+          access_id:        @access_id,
+          access_pwd:       @access_pwd,
+          sCallerId:        @sender_fax_number,
+          sSenderEmail:     @sender_email,
+          sFaxType:         'SINGLE', # TODO: make this "SINGLE" or "BROADCAST" optionally
+          sToFaxNumber:     to,
+          sResponseFormat:  'JSON',
+          sRetries:         3,
+          sFileName_1:      'preferral_file',
+          sFileContent_1:   file
+        })
+    end
   end
 end
